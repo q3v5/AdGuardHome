6715105e129ab0a081e551cdd81763f585938e89
zph224 <zph224@users.noreply.github.com>
2022-03-20 21:50:34 +0800

    Add files via upload

---
+++ rpi-replace-apt-source-buster.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "开始替换树莓派软件源到 tuna 镜像"
+
+echo "replace /etc/apt/sources.list"
+cat << EOF > /etc/apt/sources.list
+deb [arch=armhf] http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
+deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
+deb [arch=arm64] http://mirrors.tuna.tsinghua.edu.cn/raspbian/multiarch/ buster main
+EOF
+
+echo "replace /etc/apt/sources.list.d/raspi.list"
+cat << EOF > /etc/apt/sources.list.d/raspi.list
+deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui
+EOF
+
+echo "[电气罐头] 替换完成"
+echo "使用文档：https://tech.biko.pub/#/tool/rpi-apt-sources"
