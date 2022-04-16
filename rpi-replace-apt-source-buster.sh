2f7bb6ff96f8153f69f425b2cd050c43d69d3614
zph224 <zph224@users.noreply.github.com>
2022-04-16 22:38:09 +0800

    Update rpi-replace-apt-source-buster.sh

--- rpi-replace-apt-source-buster.sh
+++ rpi-replace-apt-source-buster.sh
@@ -6,14 +6,14 @@ echo "开始替换树莓派软件源到 tuna 镜像"
 
 echo "replace /etc/apt/sources.list"
 cat << EOF > /etc/apt/sources.list
-deb [arch=armhf] http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
-deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
-deb [arch=arm64] http://mirrors.tuna.tsinghua.edu.cn/raspbian/multiarch/ buster main
+deb [arch=armhf] http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi
+deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi
+deb [arch=arm64] http://mirrors.tuna.tsinghua.edu.cn/raspbian/multiarch/ bullseye main
 EOF
 
 echo "replace /etc/apt/sources.list.d/raspi.list"
 cat << EOF > /etc/apt/sources.list.d/raspi.list
-deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui
+deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ bullseye main ui
 EOF
 
 echo "[电气罐头] 替换完成"
