31fbd380d023951ee326d799abaad4395a93dcd1
zph224 <zph224@users.noreply.github.com>
2022-04-16 23:21:22 +0800

    Update rpi-replace-apt-source-buster.sh

--- rpi-replace-apt-source-buster.sh
+++ rpi-replace-apt-source-buster.sh
@@ -13,7 +13,7 @@ EOF
 
 echo "replace /etc/apt/sources.list.d/raspi.list"
 cat << EOF > /etc/apt/sources.list.d/raspi.list
-deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ bullseye main 
+deb http://archive.raspbian.org/raspbian bullseye main 
 EOF
 
 echo "[电气罐头] 替换完成"
