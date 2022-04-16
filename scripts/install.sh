08dcbca095a1b1104680d7e56222983a1a60cea2
zph224 <zph224@users.noreply.github.com>
2022-04-16 20:31:26 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -381,7 +381,7 @@ configure() {
 	check_out_dir
 
 	pkg_name="AdGuardHome_${os}_${cpu}.${pkg_ext}"
-	url="http://192.168.1.15/chfs/shared/AdGuardHome_linux_arm64.tar.gz"
+	url="https://ghproxy.fsofso.com/https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.6/AdGuardHome_linux_arm64.tar.gz"
 	agh_dir="${out_dir}/AdGuardHome"
 	readonly pkg_name url agh_dir
 
