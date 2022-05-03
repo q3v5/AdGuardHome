ef3ffa0cbff3828a64c5f27582a0666cc671882b
zph224 <zph224@users.noreply.github.com>
2022-05-03 16:51:13 +0800

    Update rpi-replace-apt-source-buster.sh

--- rpi-replace-apt-source-buster.sh
+++ rpi-replace-apt-source-buster.sh
@@ -6,15 +6,22 @@ echo "开始替换树莓派软件源到 tuna 镜像"
 
 echo "replace /etc/apt/sources.list"
 cat << EOF > /etc/apt/sources.list
-deb [arch=armhf] http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi
-deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi
-deb [arch=arm64] http://mirrors.tuna.tsinghua.edu.cn/raspbian/multiarch/ bullseye main
+#deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi
+#deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi
+#deb [arch=arm64] http://mirrors.tuna.tsinghua.edu.cn/raspbian/multiarch/ bullseye main
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
+deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free
+deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free
+deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free
+deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free
 EOF
 
 echo "replace /etc/apt/sources.list.d/raspi.list"
 cat << EOF > /etc/apt/sources.list.d/raspi.list
 deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ bullseye main 
-deb-src http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ bullseye main 
 EOF
 
 echo "[电气罐头] 替换完成"
